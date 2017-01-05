43bbdc91b22440db9e3b41e4742f96986e69f15c
Paweł Lasek <pawel.lasek@gmail.com>
2017-01-05 16:35:12 +0000

    Added EDID parsing and output of monitor name, manufacturer and serial

--- get-displays.go
+++ get-displays.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
-import "github.com/jochenvg/go-udev"
+import (
+	"fmt"
+	"github.com/anoopengineer/edidparser/edid"
+	"github.com/jochenvg/go-udev"
+	"io/ioutil"
+	"log"
+	"strings"
+)
+
+func printEdidBytes(edid []byte) {
+	fmt.Println("EDID dump")
+	columnCounter := 0
+	for i := 0; i < len(edid); i++ {
+		fmt.Printf("%02X ", edid[i])
+		if columnCounter == 15 {
+			fmt.Println()
+			columnCounter = 0
+		} else {
+			columnCounter++
+		}
+	}
+}
 
 func main() {
 	u := udev.Udev{}
@@ -12,7 +32,20 @@ func main() {
 	for i := range devices {
 		device := devices[i]
 		if device.SysattrValue("status") == "connected" {
-			fmt.Println(device.Syspath())
+			devicePath := strings.Split(device.Syspath(), "/")
+			deviceName := devicePath[len(devicePath)-1]
+			// fmt.Println(device.Syspath())
+			bs, err := ioutil.ReadFile(device.Syspath() + "/edid")
+			if err != nil {
+				log.Fatal("Unable to read EDID from ", device.Syspath(), err)
+			}
+			// printEdidBytes(bs)
+			e, err := edid.NewEdid(bs)
+			if err != nil {
+				log.Fatal("Unable to parse EDID ", err)
+			} else {
+				fmt.Println(strings.Join([]string{deviceName, e.ManufacturerId, e.MonitorName, e.MonitorSerialNumber}, ":"))
+			}
 		}
 	}
 }
